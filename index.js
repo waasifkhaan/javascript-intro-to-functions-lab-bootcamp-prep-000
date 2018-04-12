487e95e0b471662ad24e43059aa6b909f6c3c6e3
Wasif Khan <waasif.khaan@gmail.com>
2018-04-12 16:11:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function shout(string) {
   return string.toUpperCase()
 };
@@ -22,3 +23,30 @@ function sayHiToGrandma(string) {
         return 'I love you, too.';
         }
     };
+=======
+function shout (string){
+  return string.toUpperCase()
+}
+
+function whisper(string){
+
+return string.toLowerCase()
+}
+/*
+funtion logShout(string.toUpperCase){
+  return
+}
+*//*
+funtion sayHiToGrandma(string){
+  if(string.toLowerCase===string)
+    console.log('I can\'t hear you!')
+    if (string.toUpperCase===string)
+      console.log('YES INDEED!')
+      if(string==="I love you, Grandma.")
+      console.log('I love you, too.')
+    }*/
+
+            function add(x, y) {
+              return x + y
+            }
+>>>>>>> fe55619ea52f7708ad7fd9537f3bbf2236c2f08b
